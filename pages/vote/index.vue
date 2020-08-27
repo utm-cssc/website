a319984ae459c0a1d7719f306dcd906bbd752a4f
Yu Heng Su <yuheng.su@mail.utoronto.ca>
2020-08-27 14:59:10 -0400

    Started the weighted vote system

--- pages/vote/index.vue
+++ pages/vote/index.vue
@@ -7,17 +7,45 @@ export default {
   data () {
     return {
       options: {},
-      series: [44, 55, 41, 17, 15]
+      series: [44, 55, 41, 17, 15],
+      vote: ['a', 'b', 'c'],
+      first: '',
+      second: '',
+      counter: 0
     }
   },
   methods: {
-    submitVote () {
-      const radioButtons = document.getElementsByName('favorite_pet')
-      for (let i = 0; i < radioButtons.length; i++) {
-        if (radioButtons[i].checked === true) {
-          alert(radioButtons[i].value)
-        }
+    setUp () {
+      const container = document.querySelector('#listContainer')
+      for (let i = 0; i < this.vote.length; i++) {
+        const listItem = document.createElement('li')
+        // const subText = document.createElement('a')
+        listItem.innerHTML = this.vote[i]
+        container.appendChild(listItem)
       }
+    },
+    submitVote (name) {
+      // const radioButtons = document.getElementsByName('favorite_pet')
+      // for (let i = 0; i < radioButtons.length; i++) {
+      //   if (radioButtons[i].checked === true) {
+      //     alert(radioButtons[i].value)
+      //   }
+      // }
+
+      const test = document.getElementById(name + 'Name')
+
+      if (this.counter === 2) {
+        this.counter = 1
+      } else {
+        this.counter += 1
+      }
+      if (this.counter === 1) {
+        this.first = name
+      } else {
+        this.second = name
+      }
+      test.innerHTML = this.counter
+      alert(this.first + ' ' + this.second)
     }
   }
 }
@@ -28,18 +56,32 @@ export default {
   <div class="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto mt-5">
     <h1>Vote</h1>
     <article>
-      <div class="flex-col mr-2">
-        <fieldset>
+      <div class="flex-col mr-2" id="container">
+        <ul id="listContainer">
+          <li @click="submitVote('a')" id="a">
+            A
+            <a id="aName"></a>
+          </li>
+          <li @click="submitVote('b')" id="b">
+            B
+            <a id="bName"></a>
+          </li>
+          <li @click="submitVote('c')" id="c">
+            C
+            <a id="cName"></a>
+          </li>
+        </ul>
+        <!-- <fieldset>
           <h2>What is Your Favorite Pet?</h2>
           <input type="radio" name="favorite_pet" value="Cats" checked />Cats<br />
           <input type="radio" name="favorite_pet" value="Dogs" />Dogs<br />
           <input type="radio" name="favorite_pet" value="Birds" />Birds<br />
           <button type="button" @click="submitVote()">Click Me!</button>
-        </fieldset>
+        </fieldset> -->
       </div>
       <!-- Display result here, can use bar graph or pie char -->
     </article>
-      <no-ssr> <apexcharts width="380" type="donut" :options="options" :series="series"></apexcharts> </no-ssr>
+    <no-ssr> <apexcharts width="380" type="donut" :options="options" :series="series"></apexcharts> </no-ssr>
 
   </div>
 </template>
--- plugins/vue-apexchart.js
+++ plugins/vue-apexchart.js
@@ -3,4 +3,5 @@ import VueApexCharts from 'vue-apexcharts'
 
 // npm install --save apexcharts
 // npm install --save vue-apexcharts
+
 Vue.component('apexcharts', VueApexCharts)
