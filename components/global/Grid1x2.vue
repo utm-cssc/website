1726b3bc52e03050ae1960dbf0840f33a47fdbd3
Chris <hello@hiimchrislim.co>
2020-08-27 12:22:56 -0400

    Fix Grid1x2 styling

--- components/global/Grid1x2.vue
+++ components/global/Grid1x2.vue
@@ -2,27 +2,39 @@
   <div class="mb-5">
     <div class="row d-flex align-items-center">
       <div
-        class="col-md-5 col-lg-4 py-5 d-flex justify-content-start"
-        :class="{'order-md-last': !reversed, 'justify-content-md-end': !reversed, 'px-5': $md == 'sm'}"
+        class="col-md-5 col-lg-4 d-flex justify-content-start"
+        :class="{
+          'order-md-last': !reversed,
+          'justify-content-md-end': !reversed,
+          'px-5': $md == 'sm'
+        }"
       >
-        <img
-          class="feature-img"
-          :src="imgSrc"
-        >
+        <img class="feature-img" :src="imgSrc" />
       </div>
-      <div class="col-md-7 col-lg-8 d-flex justify-content-center flex-column text-container" :class="{'px-5': $mq == 'sm', 'align-items-md-end': reversed}">
+      <div
+        class="col-md-7 col-lg-8 d-flex justify-content-center flex-column text-container"
+        :class="{
+          'px-5': $mq == 'sm',
+          'align-items-md-end': reversed,
+          'left-padded-desc': reversed,
+          'right-padded-desc': !reversed
+        }"
+      >
         <h3 v-if="title != ''" class="feature-title">
           {{ title }}
         </h3>
-        <p class="desc" :class="{'text-md-right': reversed && !($mq == 'md') && !($mq == 'sm')}">
+        <p
+          class="desc"
+          :class="{
+            'text-md-right': reversed && !($mq == 'md') && !($mq == 'sm')
+          }"
+        >
           {{ desc }}
         </p>
         <div>
-          <b-btn
-            v-if="link"
-            :href="link"
-            class="p-3 feature-btn"
-          >{{ button }}</b-btn>
+          <b-btn v-if="link" :href="link" class="py-2 px-3 feature-btn">{{
+            button
+          }}</b-btn>
         </div>
       </div>
     </div>
@@ -75,8 +87,8 @@ export default {
   font-weight: 300;
   line-height: 1.75rem;
   opacity: 0.9;
-  font-size: 28px;
-  margin-top: 20px;
+  font-size: 24px;
+  margin-top: 0;
 }
 
 .feature-title {
@@ -84,6 +96,14 @@ export default {
   line-height: 1.25;
   font-size: 42px;
   color: var(--color-text-dark);
+}
+
+.right-padded-desc {
+  padding-right: 48px;
+}
+
+.left-padded-desc {
+  padding-left: 48px;
 }
 
 @media screen and (max-width: 768px) {
--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,7 @@ export default {
   ** Global CSS
   */
   css: [
+    '@/assets/main.css'
   ],
   /*
   ** Plugins to load before mounting the App
