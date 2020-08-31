f6f2e8ebffb1e1a75497b4543723a54840b0fa43
Chris <hello@hiimchrislim.co>
2020-08-31 12:50:14 -0400

    Add landing page progress

--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,16 @@
       <img src="@/assets/logo/cssc-logo.png" alt="UTM CSSC Logo">
       <b-btn class="mt-4">Tech Of the Month →</b-btn>
     </div>
-
+    <Grid-1-X-3 :children="features"></Grid-1-X-3>
     <!-- What is CSSC -->
     <div>
       <div class="cssc-heading">What is CSSC?</div>
       <p class="cssc-subheading">
         The Computer Science Student Community (CSSC) is an open space for Computer Science students to come in and ask questions about their university and post-university career. We act as liaisons to the Computer Science faculty to ensure that all students can seek appropriate support and guidance. The CSSC seeks to support students through community initiatives built upon inclusivity and empowerment.
       </p>
-      <Grid-1-X-3 :children="features"></Grid-1-X-3>
+    </div>
+    <div>
+      <div class="cssc-heading">How to contribute?</div>
     </div>
   </div>
 </template>
@@ -46,11 +48,13 @@ export default {
 }
 
 .cssc-heading {
-  font-size: 96px;
+  color: #00d097;
+  font-size: 50px;
+  font-weight: bold;
 }
 
 .cssc-subheading {
-  font-size: 32px;
+  font-size: 30px;
 }
 
 @media screen and (max-width: 768px) {
