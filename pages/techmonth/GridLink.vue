c54a3042dfbfc6f902431257d7ed7dbd6a98a6fb
Yu Heng Su <yuheng.su@mail.utoronto.ca>
2020-09-01 12:40:47 -0400

    Added a base foundation for the tech of the month page

---
+++ pages/techmonth/GridLink.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row" id="monthly">
+    <span
+      v-for="child in children"
+      :key="child.title"
+      class="col-lg-4"
+    >
+    <grid-1-x-2 :title="child.title" :desc="child.desc" :link="child.link" button="Check it out!"></grid-1-x-2>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    children: Array
+  }
+}
+</script>
