8e6e6059a82b693e41a0c95ca23a21ad138db55a
Chris <hello@hiimchrislim.co>
2020-09-24 20:05:32 -0400

    Add starter projects page

---
+++ pages/projects/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="mt-5 d-flex flex-column justify-content-center align-items-center">
+      <CenteredHero
+        icon="../../icons/projects.svg"
+        title="Projects"
+        desc="Discover student and community made projects!"
+        arrowLink="#projects"
+      />
+    </div>
+    <a id="projects" />
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
