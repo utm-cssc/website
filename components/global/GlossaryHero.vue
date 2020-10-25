b10e3b13aa97750e4fba931b14821d7c674ae096
ArsalaBangash <arsalabangash@gmail.com>
2020-10-25 19:06:06 +0500

    Completed the glossary page

---
+++ components/global/GlossaryHero.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1 class="cssc-heading text-center">Glossary</h1>
+    <vue-typed-js :strings="items">
+      <p
+        class="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto text-center"
+      >
+        What is <span class="typing"></span>?
+      </p>
+    </vue-typed-js>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        'an API',
+        'a Full Stack Developer',
+        'Big Data',
+        'a Neural Network',
+        'Vue.js',
+        'a Data Structure',
+      ],
+    }
+  },
+}
+</script>
