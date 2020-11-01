e7ce98daf7598a210edeab53e59b515ddb31861f
Chris Lim <hello@hiimchrislim.co>
2020-11-01 01:48:10 -0400

    Display scrollbar only on overflown elements

--- components/global/SideBar.vue
+++ components/global/SideBar.vue
@@ -10,7 +10,7 @@
           class="block mt-1 text-sm scrollactive-item transition-transform ease-in-out duration-300 transform hover:translate-x-2"
           :class="{
             'py-2 subheading': tocItem.depth === 2,
-            'ml-2 pb-2 innerSubheading': tocItem.depth === 3
+            'ml-2 pb-2 innerSubheading': tocItem.depth === 3,
           }"
         >
           {{ tocItem.text }}
@@ -24,59 +24,59 @@
 export default {
   props: {
     title: String,
-    toc: Array
-  }
+    toc: Array,
+  },
 }
 </script>
 
 <style scoped>
-  li {
-    list-style-type: none;
-  }
+li {
+  list-style-type: none;
+}
 
-  .heading {
-    font-weight: bold;
-    font-size: 30px;
-  }
+.heading {
+  font-weight: bold;
+  font-size: 30px;
+}
 
-  .subheading {
-    font-weight: bold;
-  }
+.subheading {
+  font-weight: bold;
+}
 
-  .subheading:hover,
-  .subheading:active {
-    color: var(--color-primary);
-  }
+.subheading:hover,
+.subheading:active {
+  color: var(--color-primary);
+}
 
-  .innerSubheading:hover,
-  .innerSubheading:active {
-    color: var(--color-primary-dark);
-  }
+.innerSubheading:hover,
+.innerSubheading:active {
+  color: var(--color-primary-dark);
+}
 
-  a:hover {
-    text-decoration: none;
-  }
+a:hover {
+  text-decoration: none;
+}
 
-  .toc-container {
-    position: relative;
-  }
+.toc-container {
+  position: relative;
+}
 
+.tableOfContents {
+  max-width: 370px;
+  position: sticky;
+  overflow-y: auto;
+  overflow-x: hidden;
+  max-height: 80vh;
+  top: 0;
+}
+
+@media (max-width: 1200px) {
   .tableOfContents {
-    max-width: 350px;
-    position: sticky;
-    overflow-y: scroll;
+    max-width: 600px;
+    max-height: 100%;
+    margin: 0 30px 0 30px;
+    overflow-y: none;
     overflow-x: hidden;
-    max-height: 80vh;
-    top: 0;
   }
-
-  @media (max-width: 1200px) {
-    .tableOfContents {
-      max-width: 600px;
-      max-height: 100%;
-      margin: 0 30px 0 30px;
-      overflow-y: none;
-      overflow-x: hidden;
-    }
-  }
+}
 </style>
