449b5772ed2bb984969a9e91e008e75932821f8a
mills <milindvishnoi@gmail.com>
2020-11-06 03:32:52 +0530

    Minor fixes in DarkModeButton

--- components/global/DarkModeButton.vue
+++ components/global/DarkModeButton.vue
@@ -1,36 +1,35 @@
 <template>
   <div>
-    <Light class="logo light" v-if="isDark" @click="$colorMode.preference = 'light'" />
-    <Dark class="logo" v-else @click="$colorMode.preference = 'dark'" />
+    <LightIcon
+      class="logo light"
+      v-if="isDark"
+      @click="$colorMode.preference = 'light'"
+    />
+    <DarkIcon class="logo" v-else @click="$colorMode.preference = 'dark'" />
   </div>
 </template>
 
 <script>
-import Light from '../../static/svg/sun.svg?inline'
-import Dark from '../../static/svg/moon.svg?inline'
+import LightIcon from '@/static/svg/sun.svg?inline'
+import DarkIcon from '@/static/svg/moon.svg?inline'
 
 export default {
-  data() {
-      return {
-        colors: ['light', 'system', 'dark']
-      }
-  },
   computed: {
     isDark() {
       return this.$nuxt.$colorMode.value == 'dark'
-    }
+    },
   },
   components: {
-    Light,
-    Dark
-  }
+    LightIcon,
+    DarkIcon,
+  },
 }
 </script>
 
 <style scoped>
 .logo {
-  width: 35px;
-  height: 35px;
+  width: 36px;
+  height: 36px;
   fill: var(--color-body);
   transition: all 0.3s ease;
 }
