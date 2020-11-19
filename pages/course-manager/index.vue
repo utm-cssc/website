5208ef5c83f9ec16d28ac6be5bb04b810cadd8ee
mills <milindvishnoi@gmail.com>
2020-11-19 23:40:40 +0530

    Refactored gpa calculator to work with multiple courses

--- pages/course-manager/index.vue
+++ pages/course-manager/index.vue
@@ -1,142 +1,134 @@
 <template>
   <div class="container">
-    <v-data-table :headers="headers" :items="tests" class="elevation-1">
-      <template v-slot:top>
-        <v-toolbar flat>
-          <v-toolbar-title>
-            Grades
-          </v-toolbar-title>
-          <v-spacer />
-          <v-dialog v-model="dialog" max-width="500px">
-            <template v-slot:activator="{on, attrs}">
-              <v-btn color="primary" text class="mb-2" v-bind="attrs" v-on="on">
-                <v-icon color="primary">
-                  mdi-plus
-                </v-icon>
-                Add Assignment
-              </v-btn>
-            </template>
-            <v-card>
-              <v-form v-model="validValues">
-                <v-card-title>
-                  <span class="headline">{{ formTitle }}</span>
+    <div v-for="course in courses" :key="course.code">
+      <h1>{{ course.code }}: {{ course.name }}</h1>
+      <br />
+      <h3>
+        In the remaining
+        <span class="font-weight-bold"
+          >{{ calculatePercentageLeft(course) }}%</span
+        >
+      </h3>
+      <div v-for="gpaDenom in gpaDenoms" :key="gpaDenom.name">
+        <div
+          v-if="calculateGradeData(course, gpaDenom.minScore).requiredScore > 0"
+        >
+          <p>
+            You need a
+            <span class="font-weight-bold"
+              >{{
+                calculateGradeData(course, gpaDenom.minScore).requiredScore
+              }}
+              % </span
+            >the course to score a {{ gpaDenom.name }}
+          </p>
+        </div>
+      </div>
+      <v-data-table
+        :headers="headers"
+        :items="course.assessments"
+        class="elevation-1"
+      >
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-toolbar-title>
+              Assessments
+            </v-toolbar-title>
+            <v-spacer />
+            <v-dialog v-model="dialog" max-width="500px">
+              <template v-slot:activator="{on, attrs}">
+                <v-btn
+                  color="primary"
+                  text
+                  class="mb-2"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  <v-icon color="primary">
+                    mdi-plus
+                  </v-icon>
+                  Add Assessment
+                </v-btn>
+              </template>
+              <v-card>
+                <v-form v-model="validValues">
+                  <v-card-title>
+                    <span class="headline">{{ formTitle }}</span>
+                  </v-card-title>
+
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="itemUnderEdit.name"
+                            label="Assessment Name"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="itemUnderEdit.grade"
+                            label="Grade"
+                            :rules="scoreRules"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="itemUnderEdit.weight"
+                            label="Weight"
+                            :rules="scoreRules"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer />
+                    <v-btn color="primary" text @click="close">
+                      Cancel
+                    </v-btn>
+                    <v-btn
+                      color="primary"
+                      text
+                      @click="save(course)"
+                      :disabled="!validValues"
+                    >
+                      Save
+                    </v-btn>
+                  </v-card-actions>
+                </v-form>
+              </v-card>
+            </v-dialog>
+            <v-dialog v-model="dialogDelete" max-width="500px">
+              <v-card>
+                <v-card-title class="headline">
+                  Are you sure you want to delete this item?
                 </v-card-title>
-
-                <v-card-text>
-                  <v-container>
-                    <v-row>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field
-                          v-model="editedItem.test_name"
-                          label="Assignment Name"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field
-                          v-model="editedItem.grade"
-                          label="Grade"
-                          :rules="scoreRules"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field
-                          v-model="editedItem.weightage"
-                          label="Weightage"
-                          :rules="scoreRules"
-                        ></v-text-field>
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                </v-card-text>
-
                 <v-card-actions>
                   <v-spacer />
-                  <v-btn color="primary" text @click="close">
-                    Cancel
-                  </v-btn>
-                  <v-btn
-                    color="primary"
-                    text
-                    @click="save"
-                    :disabled="!validValues"
+                  <v-btn color="primary" text @click="closeDelete"
+                    >Cancel</v-btn
                   >
-                    Save
+                  <v-btn color="primary" text @click="deleteItemConfirm">
+                    OK
                   </v-btn>
+                  <v-spacer />
                 </v-card-actions>
-              </v-form>
-            </v-card>
-          </v-dialog>
-          <v-dialog v-model="dialogDelete" max-width="500px">
-            <v-card>
-              <v-card-title class="headline">
-                Are you sure you want to delete this item?
-              </v-card-title>
-              <v-card-actions>
-                <v-spacer />
-                <v-btn color="primary" text @click="closeDelete">Cancel</v-btn>
-                <v-btn color="primary" text @click="deleteItemConfirm">
-                  OK
-                </v-btn>
-                <v-spacer />
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-toolbar>
-      </template>
-      <template v-slot:item.actions="{item}">
-        <v-icon small class="mr-2" @click="editItem(item)">
-          mdi-pencil
-        </v-icon>
-        <v-icon small @click="deleteItem(item)">
-          mdi-delete
-        </v-icon>
-      </template>
-    </v-data-table>
-    <v-form
-      v-model="validExpectedScore"
-      @submit="submit"
-      onSubmit="return false;"
-    >
-      <div class="d-flex flex-wrap justify-content-between my-5">
-        <v-text-field
-          v-model="expectedScore"
-          class="ml-lg-5"
-          label="Expected Overall Score"
-          :rules="scoreRules"
-        />
-        <v-btn
-          dark
-          color="primary"
-          :disabled="!validExpectedScore"
-          @click="calculateGrades()"
-          >Get Grades!</v-btn
-        >
-      </div>
-    </v-form>
-    <v-dialog v-model="showGrades" max-width="500px">
-      <v-card>
-        <v-card-title class="headline pb-5">Grade Summary</v-card-title>
-        <v-card-subtitle
-          >Need to score:
-          <span class="font-weight-bold">{{ scoreRequired }}%</span>
-          in the rest of
-          <span class="font-weight-bold">{{ percentLeft }}%</span>
-          to get
-          <span class="font-weight-bold">{{ expectedScore }}%</span>
-        </v-card-subtitle>
-        <v-card-text>
-          Scored: {{ currentPercent }}% <br />
-          Lost: {{ percentageLost }}% <br />
-          Percent of course completed: {{ currentPercent + percentageLost }}%
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" text @click="closeGrades">
-            OK
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+        </template>
+        <template v-slot:item.actions="{item}">
+          <v-icon small class="mr-2" @click="editItem(course, item)">
+            mdi-pencil
+          </v-icon>
+          <v-icon small @click="deleteItem(course, item)">
+            mdi-delete
+          </v-icon>
+        </template>
+      </v-data-table>
+    </div>
   </div>
 </template>
 
@@ -146,12 +138,6 @@ export default {
     validExpectedScore: true,
     validValues: false,
     alert: false,
-    scoreRequired: 0,
-    percentLeft: 0,
-    percentageLost: 0,
-    currentPercent: 0,
-    showGrades: false,
-    expectedScore: 100,
     scoreRules: [
       value =>
         (!isNaN(value) && parseInt(value) <= 100 && parseInt(value) >= 0) ||
@@ -161,16 +147,16 @@ export default {
     dialogDelete: false,
     headers: [
       {
-        text: 'Assignment Name',
-        value: 'test_name',
+        text: 'Assessment Name',
+        value: 'name',
       },
       {
         text: 'Grade',
         value: 'grade',
       },
       {
-        text: 'Weightage',
-        value: 'weightage',
+        text: 'Weight',
+        value: 'weight',
       },
       {
         text: 'Actions',
@@ -178,39 +164,120 @@ export default {
         sortable: false,
       },
     ],
-    tests: [
+    gpaDenoms: [
       {
-        test_name: 'Test 1',
-        grade: 85,
-        weightage: 20,
+        name: '4.0',
+        minScore: 85,
       },
       {
-        test_name: 'Assignment 2',
-        grade: 100,
-        weightage: 20,
+        name: '3.7',
+        minScore: 80,
       },
       {
-        test_name: 'Assignment 3',
-        grade: 90,
-        weightage: 20,
+        name: '3.3',
+        minScore: 77,
+      },
+      {
+        name: '3.0',
+        minScore: 73,
+      },
+      {
+        name: '2.7',
+        minScore: 70,
+      },
+      {
+        name: '2.3',
+        minScore: 67,
+      },
+      {
+        name: '2.0',
+        minScore: 63,
+      },
+      {
+        name: '1.7',
+        minScore: 60,
+      },
+      {
+        name: '1.3',
+        minScore: 57,
+      },
+      {
+        name: '1.0',
+        minScore: 53,
+      },
+      {
+        name: '0.7',
+        minScore: 50,
+      },
+      {
+        name: '0.0',
+        minScore: 0,
+      },
+    ],
+    courses: [
+      {
+        name: 'Introduction to Computer Science',
+        code: 'CSC148',
+        assessments: [
+          {
+            name: 'Peerwise',
+            weight: 6,
+            grade: 100,
+          },
+          {
+            name: 'Preps',
+            weight: 9,
+            grade: 100,
+          },
+          {
+            name: 'Exercise 1',
+            weight: 5,
+            grade: 100,
+          },
+          {
+            name: 'Test 1',
+            weight: 10,
+            grade: 80,
+          },
+          {
+            name: 'Assignment 1',
+            weight: 15,
+            grade: 100,
+          },
+          {
+            name: 'Exercise 2',
+            weight: 5,
+            grade: 100,
+          },
+          {
+            name: 'Test 2',
+            weight: 10,
+            grade: 84,
+          },
+          {
+            name: 'Assignment 2',
+            weight: 15,
+            grade: 90,
+          },
+        ],
       },
     ],
     editedIndex: -1,
-    editedItem: {
-      test_name: 'Assignment',
-      weightage: 0,
+    itemUnderEdit: {
+      name: '',
+      weight: 0,
       grade: 0,
     },
     defaultItem: {
-      test_name: 'Assignment',
-      weightage: 0,
+      name: '',
+      weight: 0,
       grade: 0,
     },
   }),
 
   computed: {
     formTitle() {
-      return this.editedIndex === -1 ? 'New Assignment' : 'Edit Assignment'
+      return this.editedIndex === -1 ? 'New Assessment' : 'Edit Assessment'
     },
   },
 
@@ -230,63 +297,90 @@ export default {
     closeGrades() {
       this.showGrades = false
     },
-    editItem(item) {
-      this.editedIndex = this.tests.indexOf(item)
-      this.editedItem = Object.assign({}, item)
+    editItem(course, item) {
+      this.editedIndex = course.assessments.indexOf(item)
+      this.itemUnderEdit = Object.assign({}, item)
       this.dialog = true
     },
-    deleteItem(item) {
-      this.editedIndex = this.tests.indexOf(item)
-      this.editedItem = Object.assign({}, item)
+    deleteItem(course, item) {
+      this.editedIndex = course.assessments.indexOf(item)
+      this.itemUnderEdit = Object.assign({}, item)
       this.dialogDelete = true
     },
     deleteItemConfirm() {
-      this.tests.splice(this.editedIndex, 1)
+      this.courses.assessments.splice(this.editedIndex, 1)
       this.closeDelete()
     },
     close() {
       this.dialog = false
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
+        this.itemUnderEdit = Object.assign({}, this.defaultItem)
         this.editedIndex = -1
       })
     },
     closeDelete() {
       this.dialogDelete = false
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
+        this.itemUnderEdit = Object.assign({}, this.defaultItem)
         this.editedIndex = -1
       })
     },
-    save() {
+    save(course) {
       if (this.editedIndex > -1) {
-        Object.assign(this.tests[this.editedIndex], this.editedItem)
+        // Ensure data model types are consistent
+        Object.assign(course.assessments[this.editedIndex], this.itemUnderEdit)
       } else {
-        this.tests.push(this.editedItem)
+        course.assessments.push(this.itemUnderEdit)
       }
       this.close()
     },
-    calculateGrades() {
-      const scoreWanted = parseInt(this.expectedScore)
-      this.currentPercent = 0
-      let overallWeightage = 0
-      console.log(this.tests)
-      // Loop through the grades and add all the accumulated percentages together
-      this.tests.forEach(test => {
-        const grade = parseInt(test.grade)
-        const weightage = parseInt(test.weightage)
-        // To stop from adding a string that is not number
-        if (!(isNaN(grade) || isNaN(weightage))) {
-          overallWeightage += weightage
-          this.currentPercent += (grade / 100) * weightage
-        }
+    /*
+      courseData: {
+        code:
+        name
+        assessments:
+      }
+
+      return {
+        requiredScore:
+        percentageLost:
+        percentageScored:
+        percentageLeft:
+        percentageComplete:
+      }
+    */
+    calculateGradeData(courseData, desiredScore) {
+      let currentWeight = 0
+      let currentPercentage = 0
+      courseData.assessments.forEach(assessment => {
+        const grade = parseInt(assessment.grade)
+        const weight = parseInt(assessment.weight)
+        currentWeight += weight
+        currentPercentage += (grade / 100) * weight
       })
       // Overall Percentages
-      this.percentLeft = 100 - overallWeightage
-      this.percentageLost = overallWeightage - this.currentPercent
-      this.scoreRequired =
-        (scoreWanted - this.currentPercent) * (100 / this.percentLeft)
-      this.showGrades = true
+      const percentLeft = 100 - currentWeight
+      const percentageLost = currentWeight - currentPercentage
+
+      const scoreRequired = (
+        (desiredScore - currentPercentage) *
+        (100 / percentLeft)
+      ).toFixed(2)
+      return {
+        requiredScore: scoreRequired,
+        percentageLost: percentageLost,
+        percentageScored: currentPercentage,
+        percentageLeft: percentLeft,
+        percentageComplete: percentageLost + currentPercentage,
+      }
+    },
+    calculatePercentageLeft(course) {
+      const totalPercent = course.assessments.reduce((total, assessment) => {
+        console.log(assessment.weight)
+        return total + parseInt(assessment.weight)
+      }, 0)
+      console.log('totalPercent ' + totalPercent)
+      return 100 - totalPercent
     },
   },
 }
