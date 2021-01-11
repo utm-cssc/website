6fc6d0a87a7014b12de6c6af8a3062bd1301b7d0
Chris <hello@hiimchrislim.co>
2021-01-10 22:09:09 -0500

    Add treasure hunt page

---
+++ pages/treasure-hunt/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="mt-5 pb-5">
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSd8DNcHsKOrxOpD-n6LMSOL_t1lYjTngnci_d5EVy8fpXSIXQ/viewform"
+      width="100%"
+      height="1090"
+      >Loading…</iframe
+    >
+  </div>
+</template>
