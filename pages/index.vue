368ce0f7ef0c13536a4e344c55fe6f4e9dd6cada
Chris Lim <hello@hiimchrislim.co>
2021-01-18 12:25:18 -0500

    Update WW landing page button

--- pages/index.vue
+++ pages/index.vue
@@ -80,7 +80,7 @@ export default {
         },
       ],
       button1: {
-        label: 'Winter Welcome Names',
+        label: 'Winter Welcome Guestbook',
         link: 'resources/winter-welcome',
       },
       button2: {
