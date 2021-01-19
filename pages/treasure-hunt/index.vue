7979af5a2a81a319313937641edec45b616ff077
Chris Lim <hello@hiimchrislim.co>
2021-01-19 00:35:20 -0500

    Update Treasure Hunt

--- pages/treasure-hunt/index.vue
+++ pages/treasure-hunt/index.vue
@@ -3,8 +3,11 @@
     Note: Use the Google Chrome browser or click
     <a href="https://forms.gle/b4Udh8zd3Ej2Th1q7">here</a> if the form doesn't
     load
-    <iframe src="https://forms.gle/b4Udh8zd3Ej2Th1q7" width="100%" height="1090"
-      >Loading…</iframe
-    >
+    <iframe
+      src="https://formfacade.com/headless/110588055100354964810/home/form/1FAIpQLSd8DNcHsKOrxOpD-n6LMSOL_t1lYjTngnci_d5EVy8fpXSIXQ"
+      frameBorder="0"
+      width="100%"
+      height="1000px"
+    ></iframe>
   </div>
 </template>
