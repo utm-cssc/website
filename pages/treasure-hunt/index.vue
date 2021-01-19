8e8e338e5e2d26e6711802682c2efc1097f4801c
Chris Lim <hello@hiimchrislim.co>
2021-01-19 00:38:27 -0500

    Update treasure hunt

--- pages/treasure-hunt/index.vue
+++ pages/treasure-hunt/index.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="mt-5 pb-5">
-    Note: Use the Google Chrome browser or click
-    <a href="https://forms.gle/b4Udh8zd3Ej2Th1q7">here</a> if the form doesn't
-    load
     <iframe
       src="https://formfacade.com/headless/110588055100354964810/home/form/1FAIpQLSd8DNcHsKOrxOpD-n6LMSOL_t1lYjTngnci_d5EVy8fpXSIXQ"
       frameBorder="0"
