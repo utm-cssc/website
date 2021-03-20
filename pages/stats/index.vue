8f93d7d15d8bc35fec154b7ec9713da6437deb18
Chris Lim <hello@hiimchrislim.co>
2021-03-20 19:35:36 -0400

    Add Plausible Analytics

---
+++ pages/stats/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <iframe
+      plausible-embed
+      src="https://plausible.io/cssc.utm.utoronto.ca?embed=true&theme=light"
+      scrolling="no"
+      frameborder="0"
+      loading="lazy"
+      style="width: 1px; min-width: 100%; height: 1600px;"
+    ></iframe>
+    <div style="font-size: 14px; padding-bottom: 14px;">
+      Stats powered by
+      <a
+        target="_blank"
+        style="color: #4f46e5; text-decoration: underline;"
+        href="https://plausible.io"
+        >Plausible Analytics</a
+      >
+    </div>
+    <script async src="https://plausible.io/js/embed.host.js"></script>
+  </div>
+</template>
