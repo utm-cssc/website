45407bc30649342c50b92a7eb2c2fd96acee86d5
Chris Lim <hello@hiimchrislim.co>
2021-03-25 14:33:47 -0400

    Add spatial-skills page

---
+++ pages/spatial-skills/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <iframe
+      src="http://badgepack.utm.utoronto.ca:3000"
+      frameborder="0"
+      loading="lazy"
+      style="width: 100%; height: 1100px;"
+    ></iframe>
+  </div>
+</template>
