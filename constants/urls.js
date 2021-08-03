0b71806cb08e3eba8c666ee6b43eddaa6e37ca4d
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2021-08-03 18:42:06 -0400

    update csv url

--- constants/urls.js
+++ constants/urls.js
@@ -1,5 +1,5 @@
 export const IMPORTANT_DATES_CSV =
-  'https://raw.githubusercontent.com/utm-cssc/website/integrate-calendar/content/important-dates/important_dates.csv'
+  'https://raw.githubusercontent.com/utm-cssc/website/master/content/important-dates/important_dates.csv'
 
 export const GITHUB_CLUB_FORM_RESPONSES =
   'https://spreadsheets.google.com/feeds/list/1KxxUGm1z_w0lDFkDbFhdSY2coxIGeZQMQbAwqzPA-4o/1/public/values?alt=json'
