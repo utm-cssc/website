70e35ff9751ffbc0cc2191bde805bf99d02874fc
Jarrod Servilla <jarrod.servilla@gmail.com>
2021-08-06 08:39:10 -0400

    change to %

--- components/global/CenteredThreeColumnGrid.vue
+++ components/global/CenteredThreeColumnGrid.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" style="margin-top: 230px; margin-bottom: 230px;">
+  <div class="row" style="margin-top: 15%; margin-bottom: 15%;">
     <div
       v-for="child in children"
       :key="child.title"
